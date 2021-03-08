31a17d053bacfcc679f515b21fde3daff5a41544
Tema <Tema@MacBook-Pro-Zaharova.local>
2021-03-08 02:15:14 +0200

    add table with results

--- src/modules/components/SearchResultTable/SearchResultTable.tsx
+++ src/modules/components/SearchResultTable/SearchResultTable.tsx
@@ -6,13 +6,39 @@ interface SearchResultTableInterface {
   searchResults?: DataSearchGithub;
 }
 
+enum TableItems {
+  name,
+  id,
+  html_url,
+  stargazers_count
+}
+
 const SearchResultTable: React.FC<SearchResultTableInterface> = ({ searchResults }) => {
-  console.log(searchResults);
+  if (!searchResults) {
+    return <div>Input search query</div>
+  }
 
   return(
-    <div>
-      table
-    </div>
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Url</th>
+          <th>Stars</th>
+        </tr>
+      </thead>
+      <tbody>  
+        {searchResults?.items.map( ({ name, id, html_url, stargazers_count}) => {
+          return (
+            <tr key={id}>
+              <td>{name}</td>
+              <td>{html_url}</td>
+              <td>{stargazers_count}</td>
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
   )
 }
 
